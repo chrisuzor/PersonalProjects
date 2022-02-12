bdeb8d86d5b62b54d3bd0b343754237bc7807269
Christopher <christopher@ADMINISTRATORs-MacBook-Pro-2.local>
2022-02-12 04:43:45 +0100

    Telegram Bot

--- boto3_aws/boto3_guide.py
+++ boto3_aws/boto3_guide.py
@@ -1,26 +1,25 @@
-import boto3
 import uuid
+
+import boto3
 
 
 def create_bucket_name(bucket_prefix):
-    return ''.join([bucket_prefix, str(uuid.uuid4())])
+    return "".join([bucket_prefix, str(uuid.uuid4())])
 
 
 def create_bucket(bucket_prefix, s3_connection):
     session = boto3.session.Session()
     current_region = session.region_name
     bucket_name = create_bucket_name(bucket_prefix)
-    if current_region == 'us-east-1':
+    if current_region == "us-east-1":
         bucket_response = s3_connection.create_bucket(
             Bucket=bucket_name,
-            CreateBucketConfiguration={
-                'LocationConstraint': current_region
-            })
+            CreateBucketConfiguration={"LocationConstraint": current_region},
+        )
     else:
         bucket_response = s3_connection.create_bucket(
             Bucket=bucket_name,
-            CreateBucketConfiguration={
-                'LocationConstraint': current_region
-            })
+            CreateBucketConfiguration={"LocationConstraint": current_region},
+        )
     print(bucket_name, current_region)
     return bucket_name, bucket_response
